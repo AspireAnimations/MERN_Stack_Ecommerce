c00a123251ff5372b2c54f43f8550746cfce57e0
Amarpreet Singh <animate@aspireanimations.com>
2021-08-21 17:37:12 +0530

    second push

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return <header>
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
   <Container>
-    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+    <LinkContainer to='/'>
+    <Navbar.Brand>ProShop</Navbar.Brand>
+    </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav" className="w-100">
       <Nav className="ms-auto">
-        <Nav.Link href="/cart"><i className='fa fa-shopping-cart'></i>Cart</Nav.Link>
-        <Nav.Link href="/login"><i className='fa fa-user'></i>Sign In</Nav.Link>
-        
+      <LinkContainer to='/cart'>
+        <Nav.Link><i className='fa fa-shopping-cart'></i>Cart</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/login'>
+        <Nav.Link><i className='fa fa-user'></i>Sign In</Nav.Link>
+        </LinkContainer>
         
       </Nav>
     </Navbar.Collapse>
