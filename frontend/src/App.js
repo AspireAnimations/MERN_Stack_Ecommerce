18488cb45f8ea27342d0c9055fe6e0f1a8095989
Amarpreet Singh <animate@aspireanimations.com>
2021-08-21 15:19:56 +0530

    MERN_STACK_ECOMMERCE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
+import { Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
-const App =() => {
+
+const App = () => {
   return (
-    <div className="App">
       <>
-        <h1>Welcome To Proshop</h1>
+      <Header/>
+      <main className ='py-3'>
+        <Container>
+         <HomeScreen />
+        </Container>
+        </main>
+        <Footer/>
       </>
-    </div>
   );
 }
 
